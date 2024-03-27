76006ac96ea7acc0d8cd1ac7318e5615a931f8ea
Tiago Lohan <tiiagolohan@gmail.com>
2024-03-27 11:50:45 -0300

    connect database to api

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const db = require("./db");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/categories", (req, res) => {
+  db.query("SELECT * FROM categories", (error, response) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.status(200).json(response.rows);
+  });
+});
+
+app.listen(port, () => {
+  db.connect()
+    .then(() => {
+      console.log("DB conected 👍");
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+  console.log(`Server on port ${port} 🚀`);
+});
