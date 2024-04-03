3908c5eea1feda9add7c2a57800853c13e50b746
Tiago Lohan <tiiagolohan@gmail.com>
2024-04-03 12:32:37 -0300

    feat: implement delete category

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -2,35 +2,80 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/", (req, res) => {
-  db.query("SELECT * FROM categories", (error, response) => {
-    if (error) {
-      return res.status(500).json(error);
-    }
+const findOne = (id) => {
+  return {
+    name: "fetch-category",
+    text: "SELECT * FROM categories WHERE id = $1",
+    values: [Number(id)],
+  };
+};
 
-    return res.status(200).json(response.rows);
-  });
+router.get("/", (req, res) => {
+  try {
+    db.query("SELECT * FROM categories", (error, response) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+
+      return res.status(200).json(response.rows);
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
-  const { name } = req.body;
+  try {
+    const { name } = req.body;
 
-  if (name.length < 3) {
-    return res
-      .status(400)
-      .json({ error: "Name should have more than 3 characters" });
-  }
-
-  const text = "INSERT INTO categories(name) VALUES($1) RETURNING *";
-  const values = [name];
-
-  db.query(text, values, (error, response) => {
-    if (error) {
-      return res.status(500).json(error);
+    if (name.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Name should have more than 3 characters" });
     }
 
-    return res.status(200).json(response.rows);
-  });
+    const text = "INSERT INTO categories(name) VALUES($1) RETURNING *";
+    const values = [name];
+
+    db.query(text, values, (error, response) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+
+      return res.status(200).json(response.rows);
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "Param id is mandatory." });
+    }
+
+    const query = findOne(id);
+    const category = await db.query(query);
+
+    if (!category.rows[0]) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+
+    const text = "DELETE FROM categories WHERE id = $1 RETURNING *";
+    const values = [Number(id)];
+    const deleteResponse = await db.query(text, values);
+
+    if (!category.rows[0]) {
+      return res.status(400).json({ error: "Category not deleted." });
+    }
+
+    return res.status(200).json(deleteResponse.rows);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 module.exports = router;
